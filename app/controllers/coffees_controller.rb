966438259a42edd93054802cb36ee04865567d2b
tabithatrubey <tabithatrubey@outlook.com>
2022-12-15 11:50:54 -0800

    Endpoints for Coffee Tinder

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -1,2 +1,15 @@
 class CoffeesController < ApplicationController
+def index
+    @coffees = Coffee.all
+    render json: @coffees
+end 
+def create
+    @coffee=Coffee.create(coffee_params)
+    render json: @coffee
+end
+
+private 
+def coffee_params
+    params.require(:coffee).permit(:name, :size, :milk_type, :bio, :image)
+end
 end
